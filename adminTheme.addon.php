b264baf273d608ee31c9be9de864ea17a6546301
eondcom <eond@eond.com>
2020-05-07 01:09:03 +0900

    Initial commit

---
+++ adminTheme.addon.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file adminTheme.addon.php
+ * @brief adminTheme 애드온
+ * @author 이온디(eond@eond.com)
+ */
+if(!defined('__XE__')) exit();
+if(Context::get('module') !== 'admin') return;
+
+if($called_position == 'before_display_content')
+{
+	$addon_info->skin_path = './addons/adminTheme/skins';
+	$skin = '/'.$addon_info->skin;
+	Context::addCSSFile($addon_info->skin_path.$skin.$skin.'.css');
+	return;
+}elseif($called_position != 'before_display_content') return;
